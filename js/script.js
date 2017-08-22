eb32e54d3ea4dbc9702591d70a68fc9d5216ee2d
Bruno Lima <brunolimadevelopment@gmail.com>
2017-08-22 06:41:50 -0300

    loops

--- js/script.js
+++ js/script.js
@@ -25,8 +25,8 @@ function voltaTexto(){
 }
 
 function formulario(){
-	nome = document.getElementById('nome');
-	numero = document.getElementById('numero');
+	var nome = document.getElementById('nome');
+	var varnumero = document.getElementById('numero');
 	if(nome.value == '' && numero.value == ''){
 			alert('Por Favor, preencha os campos!');
 	}else 
@@ -40,4 +40,52 @@ function formulario(){
 		alert('Dados carregados com sucesso!');
 	}
 
+}
+
+
+
+function calcularIMC(){
+	var peso = document.getElementById('peso');
+	var altura = document.getElementById('altura');
+	var calculo = altura.value * altura.value;
+	var imc = peso.value/calculo;
+	
+	if(imc < 17){
+		comentario = 'seu IMC esta muito abaixo do peso ';
+	}else 
+	if(imc >= 17 && imc <= 18){
+		comentario = 'seu IMC esta abaixo do peso ';
+	}else 
+	if(imc > 18 && imc < 25){
+		comentario = 'seu peso esta normal ';
+	}else 
+	if(imc > 25 && imc <= 30){
+		comentario = 'obsidade I ';
+	}else 
+	if(imc > 30 && imc <= 40) {
+		comentario = 'obsidade II ';		
+	}else if(imc > 40){
+		comentario = 'vc esta morto ';
+	}
+			
+	document.getElementById('resultadoImc').innerHTML = imc;		
+	document.getElementById('comentarioImc').innerHTML = comentario;		
+}
+
+function loopWhile(){
+	 x=10;
+	 z='';
+	while (x>0) {
+		z = z +'Numero: ' + x + '<br/>';
+		x--;
+	}
+	document.getElementById('while').innerHTML = z;
+}
+
+function loopFor(){
+	 j = '';
+	for(i=0; i<10; i++){
+		j = j +'Numero: ' + i + '<br/>';
+	}
+	document.getElementById('for').innerHTML = j;
 }
